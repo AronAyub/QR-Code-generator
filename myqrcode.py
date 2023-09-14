525badf6c06facbf4e9b39425bd6893fab6b5f57
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-09-14 15:56:09 +0300

    added qrs

--- myqrcode.py
+++ myqrcode.py
@@ -4,10 +4,10 @@ import qrcode as qr
 features = qr.QRCode(version=1, box_size=40, border=3)
 
 #add video link:
-features.add_data('https://liquid.tech/')
+features.add_data('For Home Internet Call 0200000000 ')
 
 #embedd details.
 features.make(fit=True)
-generate_image = features.make_image(fill_color =" ", Back_color= "white")
+generate_image = features.make_image(fill_color = 'maroon', Back_color= "white")
 #enerate_image = qr.make("Aron QR") #prints this string 
-generate_image.save('image7.png')
+generate_image.save('image1.png')
